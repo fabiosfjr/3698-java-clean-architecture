5b6e1941816ac0a897972af9f54dc61126de6f4d
Fábio Ferreira <fabio_junior1994@hotmail.com>
2024-05-18 03:21:53 -0300

    Validação de usuário menor de 18 anos e implementação dos testes unitários

--- src/main/java/br/com/alura/codechella/domain/entities/usuario/Usuario.java
+++ src/main/java/br/com/alura/codechella/domain/entities/usuario/Usuario.java
@@ -1,17 +1,28 @@
 package br.com.alura.codechella.domain.entities.usuario;
 
+import br.com.alura.codechella.domain.Endereco;
+
 import java.time.LocalDate;
+import java.time.Period;
 
 public class Usuario {
     private String cpf;
     private String nome;
     private LocalDate nascimento;
     private String email;
+    private Endereco endereco;
 
     public Usuario(String cpf, String nome, LocalDate nascimento, String email) {
         //Validar cpf
         if (cpf == null || !cpf.matches("\\d{3}\\.\\d{3}\\.\\d{3}\\-\\d{2}")) {
             throw new IllegalArgumentException("Cpf no padrão incorreto!");
+        }
+        // Calcula a idade com base na data de nascimento e a data atual
+        int idade = Period.between(nascimento, LocalDate.now()).getYears();
+
+        // Verifica se a idade é inferior a 18 anos e lança uma exceção se for
+        if (idade < 18) {
+            throw new IllegalArgumentException("Usuário deve ter pelo menos 18 anos de idade!");
         }
 
 
@@ -51,5 +62,13 @@ public class Usuario {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
     }
 }
--- src/test/java/br/com/alura/codechella/domain/entities/usuario/UsuarioTest.java
+++ src/test/java/br/com/alura/codechella/domain/entities/usuario/UsuarioTest.java
@@ -10,5 +10,36 @@ public class UsuarioTest {
     public void naoDeveCadastrarUsuarioComCpfNoFormatoInvalido() {
         Assertions.assertThrows(IllegalArgumentException.class,
                 () -> new Usuario("123456789-99", "Fábio", LocalDate.parse("1994-05-20"), "fabio_junior1994@hotmail.com"));
+
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> new Usuario("1234567899", "Fábio", LocalDate.parse("1994-05-20"), "fabio_junior1994@hotmail.com"));
+
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> new Usuario("", "Fábio", LocalDate.parse("1994-05-20"), "fabio_junior1994@hotmail.com"));
     }
+
+    @Test
+    public void deveCriarUsuarioUsandoFabricaDeUsuario() {
+        FabricaDeUsuario fabricaDeUsuario = new FabricaDeUsuario();
+
+        Usuario usuario = fabricaDeUsuario.comNomeCpfNascimento("Amanda Marques", "123.456.789-10", LocalDate.parse("1998-07-16"));
+        Assertions.assertEquals("Amanda Marques", usuario.getNome());
+
+        usuario = fabricaDeUsuario.incluiEndereco("123.456.789-10", 465, "torre 1 apto 138");
+        Assertions.assertEquals("torre 1 apto 138", usuario.getEndereco().getComplemento());
+    }
+
+    @Test
+    public void naoDeveCadastrarUsuarioComMenosDe18anos() {
+        // Crio uma data de nascimento que subtrai 17 anos da data atual
+        LocalDate dataNascimento = LocalDate.now().minusYears(17);  // Um usuário com 17 anos
+        // Crio uma exceção e atribuo a ela o resultado do teste de idade mínima, pois o correto é que a exceção seja lançada.
+        IllegalArgumentException exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            new Usuario("123.456.789-00", "Fulano", dataNascimento, "fulano@example.com");
+        });
+        // Confiro se a mensagem da exceção é a mensagem que eu esperava, referente à idade inferior
+        Assertions.assertEquals("Usuário deve ter pelo menos 18 anos de idade!", exception.getMessage());
+    }
+
+
 }
