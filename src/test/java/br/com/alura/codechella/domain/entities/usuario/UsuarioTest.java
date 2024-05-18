81e25d1ad22e5830a1c91b58cb356008e3ad3505
Fábio Ferreira <fabio_junior1994@hotmail.com>
2024-05-18 02:34:03 -0300

    Implementação do domínio Usuário e criação do teste unitário de validação de CPF

---
+++ src/test/java/br/com/alura/codechella/domain/entities/usuario/UsuarioTest.java
@@ -0,0 +1,14 @@
+package br.com.alura.codechella.domain.entities.usuario;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+public class UsuarioTest {
+    @Test
+    public void naoDeveCadastrarUsuarioComCpfNoFormatoInvalido() {
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> new Usuario("123456789-99", "Fábio", LocalDate.parse("1994-05-20"), "fabio_junior1994@hotmail.com"));
+    }
+}
